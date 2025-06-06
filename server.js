f0308d596d1644236fb2252009136f0d26a34262
WEM90 <emo333f@gmail.com>
2025-06-06 05:23:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
+app.use(cors());
 app.use(express.static('.'));
 app.use(express.json());
 
 app.post('/increment', async (req, res) => {
   try {
-    await pool.query('CREATE TABLE IF NOT EXISTS counter (id SERIAL PRIMARY KEY, count INT);');
-    const result = await pool.query('SELECT count FROM counter WHERE id=1;');
+    await pool.query('CREATE TABLE IF NOT EXISTS counter (id SERIAL PRIMARY KEY, count INT)');
+    const result = await pool.query('SELECT count FROM counter WHERE id = 1');
     if (result.rowCount === 0) {
-      await pool.query('INSERT INTO counter (id, count) VALUES (1, 1);');
+      await pool.query('INSERT INTO counter (id, count) VALUES (1, 1)');
     } else {
-      await pool.query('UPDATE counter SET count = count + 1 WHERE id=1;');
+      await pool.query('UPDATE counter SET count = count + 1 WHERE id = 1');
     }
     res.sendStatus(200);
   } catch (err) {
@@ -26,7 +29,7 @@ app.post('/increment', async (req, res) => {
 
 app.get('/get-count', async (req, res) => {
   try {
-    const result = await pool.query('SELECT count FROM counter WHERE id=1;');
+    const result = await pool.query('SELECT count FROM counter WHERE id = 1');
     const count = result.rowCount > 0 ? result.rows[0].count : 0;
     res.json({ count });
   } catch (err) {
@@ -35,4 +38,6 @@ app.get('/get-count', async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
